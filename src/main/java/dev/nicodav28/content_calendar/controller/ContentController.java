d7756e7d27fea336e69ea39bf486f629e4c5c09f
Nicodav28 <esteveznicolas0@gmail.com>
2024-10-01 10:46:35 -0500

    ✨ (ContentController.java): Añadir endpoints para buscar todos los contenidos, buscar por ID y crear un nuevo contenido ✨ (ContentCollectionRepository.java): Añadir método para inicializar la lista de contenidos con un contenido de ejemplo Se han añadido nuevos endpoints al controlador ContentController para permitir la búsqueda de todos los contenidos, búsqueda por ID y creación de un nuevo contenido. Además, se ha añadido un método de inicialización en el repositorio ContentCollectionRepository para agregar un contenido de ejemplo a la lista de contenidos al iniciar la aplicación.

--- src/main/java/dev/nicodav28/content_calendar/controller/ContentController.java
+++ src/main/java/dev/nicodav28/content_calendar/controller/ContentController.java
@@ -1,10 +1,21 @@
 package dev.nicodav28.content_calendar.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import dev.nicodav28.content_calendar.model.Content;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import dev.nicodav28.content_calendar.repository.ContentCollectionRepository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/content")
@@ -14,5 +25,21 @@ public class ContentController {
 
     public ContentController(ContentCollectionRepository contentRepository) {
         this.contentRepository = contentRepository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return contentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return contentRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        contentRepository.save(content);
     }
 }
--- src/main/java/dev/nicodav28/content_calendar/repository/ContentCollectionRepository.java
+++ src/main/java/dev/nicodav28/content_calendar/repository/ContentCollectionRepository.java
@@ -2,7 +2,11 @@ package dev.nicodav28.content_calendar.repository;
 
 import org.springframework.stereotype.Repository;
 import dev.nicodav28.content_calendar.model.Content;
+import dev.nicodav28.content_calendar.model.Status;
+import dev.nicodav28.content_calendar.model.Type;
+import jakarta.annotation.PostConstruct;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,16 +14,28 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
     }
 
+    @PostConstruct
+    private void init() {
+        Content content = new Content(1, "My firtst blog post", "My first blog post", Status.IDEA, Type.ARTICLE,
+                LocalDateTime.now(), null, "");
+
+        contentList.add(content);
+    }
+
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    public void save(Content content) {
+        contentList.add(content);
     }
 }
